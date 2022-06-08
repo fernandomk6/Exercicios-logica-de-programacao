43bce9da5ae35302774d8be681ee0bbe1a5fac3e
fernando henrique <fernandomk6@gmail.com>
2022-06-07 22:13:45 -0300

    concluido primeira sessao de exercicios.

--- 1-EstruturaSequencial/18/index.js
+++ 1-EstruturaSequencial/18/index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+const size = 1000;
+const speed = 100;
+
+const downloadTime = (size, speed) => {
+  let time = size / speed;
+  time /= 60;
+  time = time.toFixed(2);
+  let minutes = Math.floor(time);
+  let seconds = 0;
+  if (time > 1) {
+    seconds = Math.floor((time - minutes) * 60);
+  } else {
+    seconds = Math.ceil(time * 60);
+  }
+
+  return `Tempo estimado para download: ${minutes} minuto(s) e ${seconds} segundo(s)`;
+};
+
+console.log(downloadTime(size, speed));
